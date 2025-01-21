62e71fc1d74412b55a2088feb4f58d85588ac094
Madhavan M <137317994+Madhavan-012004@users.noreply.github.com>
2025-01-21 21:51:25 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,3 +4,14 @@ int main() {
     std::cout << "Hello, World!" << std::endl; // Print to screen
     return 0; // Exit the program
 }
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Vanakam" << endl;
+    return 0;
+}
