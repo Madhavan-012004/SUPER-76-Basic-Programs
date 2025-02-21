4dcebcc81ba734bc9b1b0ce0f25b0086fe223ce0
Madhavan M <137317994+Madhavan-012004@users.noreply.github.com>
2025-02-21 22:14:05 +0530

    Create str to array.cpp

---
+++ str to array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+int main() {
+    string str = "10 20 30 40 50";
+    vector<int> numbers;
+    stringstream ss(str);
+    int num;
+
+    while (ss >> num) {
+        numbers.push_back(num);
+    }
