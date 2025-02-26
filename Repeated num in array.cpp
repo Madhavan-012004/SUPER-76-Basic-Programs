3cfae8cc2731fd402da003fc8322858c4f372e40
Madhavan M <137317994+Madhavan-012004@users.noreply.github.com>
2025-02-26 21:38:01 +0530

    Create Repeated num in array.cpp

---
+++ Repeated num in array.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+int main()
+{
+    vector<int> arr;
+    string str;
+
+    getline(cin,str);
+    stringstream ss(str);
+    int num;
+
+    while (ss>>num)
+    {
+        arr.push_back(num); // making the str into arr
+    }
+    int n=arr.size();
+    bool found = false;
+    for (int i = 0; i < arr.size(); i++) {
+        for (int j = i + 1; j < arr.size(); j++) { // checking loop for the repeted num
+            if (arr[i] == arr[j]) {
+                cout << arr[i] << " ";
+                found = true; // if true
+                break;
+            }
+        }
+    }
+
+    if(!found) // no element rep it will return empt array
+    {
+        cout<<"[]";
+    }
+
+}
